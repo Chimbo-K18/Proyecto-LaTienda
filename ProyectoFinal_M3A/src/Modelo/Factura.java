461ed1d1920f60c8f214976520aef63cc18e05fc
byronn28 <byronpp16@gmail.com>
2022-03-08 17:29:15 -0500

    Modelo Factura
    Creacion del modelo de las Facturas

---
+++ ProyectoFinal_M3A/src/Modelo/Factura.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author byron
+ */
+public class Factura {
+    private int idFactura;
+    private int idCliente;
+    private int idVendedor;
+    private Date fecha;
+
+    public Factura() {
+    }
+
+    public Factura(int idFactura, int idCliente, int idVendedor, Date fecha) {
+        this.idFactura = idFactura;
+        this.idCliente = idCliente;
+        this.idVendedor = idVendedor;
+        this.fecha = fecha;
+    }
+
+    public int getIdFactura() {
+        return idFactura;
+    }
+
+    public void setIdFactura(int idFactura) {
+        this.idFactura = idFactura;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVendedor(int idVendedor) {
+        this.idVendedor = idVendedor;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+}
---
+++ ProyectoFinal_M3A/src/Modelo/Modelo_Facturas.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author byron
+ */
+public class Modelo_Facturas extends Factura{
+
+    public Modelo_Facturas() {
+    }
+    
+    /*public boolean crearFactura() {
+        String sql;
+        sql="INSERT INTO facturas(id_factura,id_cliente,id_empleado,fecha)";
+        sql+="VALUES ('" +getId_factura()+ "' , '" +getId_cliente()+ "' , '" +getId_empleado()+ "' ,'" +getFecha()+"')";
+        return cpg.accion(sql);
+
+    }
+    
+    public List <Factura> consultarFacturas(){
+        List<Factura>listaFac=new ArrayList<Factura>();
+        try {
+            
+            String sql= "SELECT * from facturas";
+            ResultSet rs= cpg.consulta(sql);
+
+            while(rs.next()){
+                Factura fac = new Factura();
+                fac.setIdFactura(rs.getInt("id_factura"));
+                fac.setIdCliente(rs.getInt("id_cliente"));
+                fac.setIdVendedor(rs.getInt("id_empleado"));
+                fac.setFecha(rs.getDate("fecha"));
+
+                listaFac.add(fac);
+            }
+            rs.close();
+            return listaFac;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Modelo_Facturas.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }*/
+}
